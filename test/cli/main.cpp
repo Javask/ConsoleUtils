2c514ea4a2238322f4dcbc4d5b6c70dc2e10a1f0
Michael Jossen <michael.jossen@rwth-aachen.de>
2022-05-03 18:41:21 +0200

    Fixed a warning

--- test/cli/main.cpp
+++ test/cli/main.cpp
@@ -4,7 +4,7 @@ int main(void) {
   ConsoleInterface cli;
   ConsoleCommand cmd;
   cmd.helpDescription = "Test Command!";
-  cmd.onExec = [](std::string cmd, std::istream& in, std::ostream& out) {
+  cmd.onExec = [](std::string cmd, std::istream&, std::ostream& out) {
     out << "TEst!\n" << cmd << "\n";
   };
   cmd.pattern = "test";
